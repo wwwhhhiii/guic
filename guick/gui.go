f3eebc636b8ac5a8c6e1d3e68ca772da19f94fd0
alexander <alexandr.s.trushnikov@gmail.com>
2026-02-09 14:50:57 +0500

    cleanup

--- guick/gui.go
+++ guick/gui.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
@@ -29,36 +28,4 @@ func NewPeerRequestElement(text string, accepted chan<- bool) *fyne.Container {
 		widget.NewButton("✔", func() { accepted <- true }),
 		widget.NewButton("✖", func() { accepted <- false }),
 	)
-}
-
-func NewChatTextGrid() *widget.TextGrid {
-	textGrid := widget.NewTextGrid()
-	textGrid.Scroll = fyne.ScrollBoth
-	return textGrid
-}
-
-type ChatWdg struct {
-	widget.BaseWidget
-	container *fyne.Container
-}
-
-func NewChatWdg() *ChatWdg {
-	return &ChatWdg{}
-}
-
-func (c *ChatWdg) CreateRenderer() fyne.WidgetRenderer {
-	container := container.NewStack()
-	return widget.NewSimpleRenderer(container)
-}
-
-func (c *ChatWdg) RecvText(s *string) error {
-	return nil
-}
-
-func (c *ChatWdg) SendText(s string) {
-	c.container.Add(canvas.NewText(s, nil))
-}
-
-func (c *ChatWdg) SendImg(img *canvas.Image) {
-
 }
